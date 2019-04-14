c64d4d60d04fdd46119af6dd7bdf27fce578b369
adeliab <adelia.benalius@gmail.com>
2019-04-14 21:55:22 +0200

    add reference to EntityFrameworkCore

---
+++ OdeToFood.Data/OdeToFoodDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToFood.Core.Model.Entities;
+
+namespace OdeToFood.Data
+{
+	public class OdeToFoodDbContext : DbContext
+	{
+		public DbSet<Restaurant> Restaurants { get; set; }
+	}
+}
